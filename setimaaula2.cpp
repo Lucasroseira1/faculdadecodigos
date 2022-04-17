1e1a21b3634f34e12c6465df77594ea598fa7f48
Lucasroseira1 <97472968+Lucasroseira1@users.noreply.github.com>
2022-04-17 12:40:22 -0300

    Add files via upload

---
+++ setimaaula2.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+float valorinvestimento; 
+int 1, 2;
+
+int main(){
+	printf("\nQual o tipo de investimento?\n\n(tipo 1 = poupanca; tipo 2 = renda fixa)\n");
+	scanf("&d""%d", &1 &2);
+	printf("\nQual e o valor do investimento?\n")
+	scanf("%f", &valorinvestimento);
+	if( 1 || 2){
+		if(1=valorinvestimento*0.03)
+		else{
+			2=(valorinvestimento*0.04);
+		}
+	}
+	else printf("Escolha invalida\n");
+	
+	printf("O seu investimento lhe trouxe %d", valorinvestimento);
+
+	return 0;
+}
